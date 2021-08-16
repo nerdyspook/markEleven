a3e7c7eb87fb7df8f50c2b9d814401a26779b75d
Susanto <nerdyspook007@gmail.com>
2021-08-16 16:37:52 +0530

    feat: Added functionality to tell whether your DOB is lucky

---
+++ js/main.js
@@ -0,0 +1,48 @@
+const dateOfBirth = document.querySelector("#DOB");
+const lucky = document.querySelector("#lucky__number");
+const check = document.querySelector("#btn-check");
+const output = document.querySelector("#output");
+const image1 = document.querySelector("#sad");
+const image2 = document.querySelector("#happy");
+
+
+function hideImage(image) {
+    image.style.display="none";
+};
+
+function showImage (image) {
+    image.style.display="block";
+}
+
+function checkIsLucky() {
+    const dob = dateOfBirth.value;
+    const sum = calculateSum(dob);
+    compareValues(sum, lucky.value);
+};
+
+function calculateSum(dob) {
+    dob = dob.replaceAll("-", "");
+
+    let sum = 0;
+    for( let i=0; i<dob.length; i++){
+        sum += Number(dob.charAt(i));
+    }
+
+    return sum; 
+};
+
+function compareValues(sum, lucky) {
+    if(sum%lucky === 0) {
+        output.innerHTML="Hurray! You are a lucky person!";
+        showImage(image2);
+    }
+    else{
+        output.innerHTML="Sorry! Your birthday is not a lucky number!";
+        showImage(image2);
+    }
+};
+
+hideImage(image1);
+hideImage(image2);
+
+check.addEventListener("click", checkIsLucky);
